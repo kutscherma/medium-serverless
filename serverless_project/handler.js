14faa8102ca8a314e92ff7d8f11e71954cb51948
Manuel Soelch <manuel.soelch@bmwiti.onmicrosoft.com>
2020-02-29 13:22:02 +0100

    Initial upload

---
+++ serverless_project/handler.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const express = require('express');
+const sls = require('serverless-http');
+const AWS = require('aws-sdk');
+const csv = require('csvtojson');
+const app = express();
+
+
+app.get('/', async (req, res, next) => {
+  
+    let S3 = new AWS.S3();
+
+    const params = {
+      Bucket: 'csvbucketmanuel',
+      Key: 'salesrecords.csv'
+    }
+
+
+    let data = async function() {
+        // get csv file and create stream
+        const stream = S3.getObject(params).createReadStream();
+        // convert csv file (stream) to JSON format data
+        const json = await csv().fromStream(stream);
+        
+        return json;
+    }
+
+      let csvData = await data();
+
+      res.status(200).send(csvData);
+
+
+});
+
+
+module.exports.server = sls(app);
